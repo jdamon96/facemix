748fb8572880dd580ea86b4f51da536d5c79f1d9
Jack Damon <jdamon96@gmail.com>
2020-03-15 17:06:32 -0700

    setting up infrastructure for making calls to other client video feeds

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 var VideoChat = {
 
+    socket: io(),
+
     requestMediaStream: function(event){
         navigator.mediaDevices
             .getUserMedia({video: true, audio: true})
@@ -17,18 +19,37 @@ var VideoChat = {
         VideoChat.localStream = stream;
         VideoChat.videoButton.setAttribute('disabled', 'disabled');
         VideoChat.localVideo.srcObject = stream;
+        VideoChat.socket.emit('join', 'test');
+        VideoChat.socket.on('ready', VideoChat.readyToCall);
+    },
+
+    readyToCall: function(event){
+        VideoChat.callButton.removeAttribute('disabled');
     },
 
     noMediaStream: function(){
         console.log('No media stream available');
+    },
+
+    startCall: function(event){
+        console.log('things are going as planned');
     }
 
 };
 
+// get references to DOM button objects
 VideoChat.videoButton = document.getElementById('get-video');
+VideoChat.callButton = document.getElementById('call');
 
+// add event listeners to DOM button objects
 VideoChat.videoButton.addEventListener(
     'click',
     VideoChat.requestMediaStream,
     false
+);
+
+VideoChat.callButton.addEventListener(
+  'click',
+  VideoChat.startCall,
+  false
 );
--- server.js
+++ server.js
@@ -12,6 +12,30 @@ var twilio = require('twilio')(
 
 app.use(express.static('public'));
 
+io.on('connection', function(socket){
+
+    socket.on('join', function(room){
+        
+        var clients = io.sockets.adapter.rooms[room];
+        var numClients = typeof clients !=='undefined' ? clients.length: 0;
+
+        if(numClients == 0){
+            socket.join(room);
+        }
+        else if (numClients == 1){
+            socket.join(room);
+            socket.emit('ready', room);
+            socket.broadcast.emit('ready', room);
+        }
+        else {
+            socket.emit('full', room);
+        }
+
+    });
+
+});
+
+
 http.listen(PORT, function() {
   console.log(`listening on ${PORT}`);
 });
